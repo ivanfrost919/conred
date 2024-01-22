3dc21d847d5201f10c1d792c72a71502c39c24b1
Flarky55 <no-reply@conred.ru>
2024-01-22 14:59:34 +0600

    Changed `gallery.js`, added image autoloading script to `loadscreen.html`

--- gallery.js
+++ gallery.js
@@ -1,16 +1,40 @@
 const carousel = document.getElementById('carousel');
 
-for (let i = 1; i <= 39; i++) {
-    let div = document.createElement('div');
-    div.classList.add('carousel-item')
-    if (i == 1) {
-        div.classList.add('active');
-    }
-    let img = document.createElement('img');
-    img.classList.add('d-block');
-    img.classList.add('w-100');
-    img.src = 'img/gallery/' + i + '.jpg';
+const PATH = "img/gallery/";
 
-    div.appendChild(img);
-    carousel.appendChild(div);
-}
+async function isValidImage(src) {
+    return new Promise((resolve) => {
+        const img = new Image()
+        img.src = src
+
+        img.onload = (a) => {
+            resolve(true)
+        }
+        img.onerror = (a) => {
+            resolve(false)
+        }
+    })
+};
+
+
+(async () => {
+    for (let i = 1; i <= 999; i++) {
+        const src = PATH + i + ".jpg";
+
+        const isValid = await isValidImage(src);
+        if (!isValid) break;
+
+        let div = document.createElement('div');
+        div.classList.add('carousel-item')
+        if (i == 1) {
+            div.classList.add('active');
+        }
+        let img = document.createElement('img');
+        img.classList.add('d-block');
+        img.classList.add('w-100');
+        img.src = src;
+
+        div.appendChild(img);
+        carousel.appendChild(div);
+    }
+})();
